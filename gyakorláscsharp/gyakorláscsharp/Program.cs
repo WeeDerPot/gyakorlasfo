d3cbf1cf5893eb585d4667aa6b67a550c07780d0
WeeDerPot <lovehansz@gmail.com>
2019-11-25 10:08:33 +0100

    fadd

---
+++ gyakorláscsharp/gyakorláscsharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyakorláscsharp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // 41. Feladat
+            for (int i = 0; i < 26; i++)
+            {
+                for (int j = 65; j < 91; j++)
+                {
+                    Console.Write((char)j);
+                }
+                for (int k = 65; k < 65+i; k++)
+                {
+                    Console.Write((char)k);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
