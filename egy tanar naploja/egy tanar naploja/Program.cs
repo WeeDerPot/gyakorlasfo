5155748af2b26e482a51799ede1f529d174dbaa6
WeeDerPot <nemeth.christopher@O16G03>
2020-02-10 12:32:16 +0100

    masodik 0210

---
+++ egy tanar naploja/egy tanar naploja/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace egy_tanar_naploja_0210
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tomb = new int[30];
+            int osszeg = 0;
+            int negativSzamok = 0;
+            List<int> paratlanok = new List<int>();
+            using (StreamReader sr = new StreamReader("szamok30.txt", Encoding.UTF8))
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    tomb[i] = Int32.Parse(sr.ReadLine());
+                    osszeg += tomb[i];
+                    if (tomb[i] < 0)
+                    {
+                        negativSzamok++;
+                    }
+                    if (tomb[i] % 2 == 1)
+                    {
+                        paratlanok.Add(tomb[i]);
+                    }
+                }
+            }
+            Console.WriteLine($"A számok összege:{osszeg}");
+            Console.WriteLine($"A negatív értékek száma:{negativSzamok}");
+            Console.WriteLine($"A páratlan számok: ");
+            foreach (var item in paratlanok)
+            {
+                Console.Write($"{item}");
+            }
+            Console.WriteLine("\nA kétszámjegyű számok: ");
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (Math.Abs(tomb[i]) > 9 && Math.Abs(tomb[i]) < 100)
+                {
+                    Console.Write($"{tomb[i]}");
+                }
+            }
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
